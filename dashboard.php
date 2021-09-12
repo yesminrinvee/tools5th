fdc1cfb6ccd535712e51dfcd08bd3fe8555ec769
Yesmin Rinvee <yesminrinvy@gmail.com>
2021-09-12 19:07:49 +0600

    init

---
+++ dashboard.php
@@ -0,0 +1,9 @@
+<h2 style="...">Dasboard!!!<br>Welcome to you</h2>
+<?php
+session_start();
+
+?>ß
+User name:(<?php echo $_SESSION['studentName']?>)<br>
+< a herf="signout.php">
+    Sign Out
+</a>
